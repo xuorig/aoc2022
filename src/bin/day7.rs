614950c26e373af9d80dfff5948bd22898336674
xuorig <mgiroux0@gmail.com>
2022-12-07 14:17:01 -0500

    cleanup

--- src/bin/day7.rs
+++ src/bin/day7.rs
@@ -1,4 +1,6 @@
-use std::{cell::RefCell, rc::Rc};
+use std::{cell::RefCell, rc::Rc, str::FromStr};
+
+
 
 fn main() {
     let input = include_str!("../inputs/7.txt");
@@ -7,11 +9,11 @@ fn main() {
     let mut current_file = root.clone();
 
     for line in input.trim().lines() {
-        if line.starts_with("$") {
-            let command = parse_command(&line);
+        let output = line.parse::<FileSystemOutputLine>().unwrap();
 
-            match command {
-                Command::Cd(dir) => match dir.as_str() {
+        match output {
+            FileSystemOutputLine::Command(command) => match command {
+                CommandLine::Cd(dir) => match dir.as_str() {
                     "/" => {},
                     ".." => {
                         let parent = current_file.borrow().parent.clone().unwrap();
@@ -21,22 +23,16 @@ fn main() {
                         let selected_file = current_file.borrow().children.iter().find(|f| f.borrow().name == dir ).expect("Directory not found").clone();
                         current_file = selected_file;
                     }
-                },
-                Command::Ls => {
-                    // NOOP
                 }
-            }
-        } else {
-            let mut split = line.split(" ");
-            let (dir_or_size, name) = (split.next().unwrap(), split.next().unwrap());
-
-            let parent_ref = current_file.clone();
-
-            if dir_or_size == "dir" {
-                current_file.borrow_mut().add_file(File { parent: Some(parent_ref), children: vec![], size: 0, name: name.to_string() });
-            } else {
-                let size = dir_or_size.parse::<u32>().unwrap();
-                current_file.borrow_mut().add_file(File { parent: Some(parent_ref), children: vec![], size, name: name.to_string() });
+                CommandLine::Ls => {}
+            },
+            FileSystemOutputLine::File(file) => match file {
+                FileLine::Dir(dir) => {
+                    current_file.borrow_mut().add_file(File { parent: Some(current_file.clone()), children: vec![], size: 0, name: dir });
+                },
+                FileLine::File(name, size) => {
+                    current_file.borrow_mut().add_file(File { parent: Some(current_file.clone()), children: vec![], size, name });
+                }
             }
         }
     }
@@ -57,7 +53,7 @@ fn main() {
         }
     }
 
-    // Paer 1
+    // Part 1
     println!("Sum: {:?}", sizes.iter().filter(|s| **s < 100000).sum::<u32>());
 
     // Part 2
@@ -69,19 +65,9 @@ fn main() {
     println!("Smallest: {:?}", smallest);
 }
 
-fn parse_command(line: &str) -> Command {
-  match &line[2..4] {
-      "cd" => Command::Cd(line[5..].to_string()),
-      "ls" => Command::Ls,
-      _ => unreachable!()
-  }
-}
-
-enum Command {
-    Cd(String),
-    Ls
-}
-
+/*
+ * File System
+ */
 type FileHandle = Rc<RefCell<File>>;
 
 struct File {
@@ -103,5 +89,67 @@ impl File {
     fn is_dir(&self) -> bool {
         self.size == 0 && self.children.len() != 0
     }
+}
 
+/*
+ * Parser
+ */
+enum FileSystemOutputLine {
+    Command(CommandLine),
+    File(FileLine)
+}
+
+enum CommandLine {
+    Cd(String),
+    Ls
+}
+
+enum FileLine {
+    Dir(String),
+    File(String, u32)
+}
+
+impl FromStr for FileSystemOutputLine {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match &s[0..1] {
+            "$" => {
+              let command = s.parse::<CommandLine>().expect("Could not parse command");
+              Ok(FileSystemOutputLine::Command(command))
+            }
+            _ => {
+              let file = s.parse::<FileLine>().expect("Could not parse file");
+              Ok(FileSystemOutputLine::File(file))
+            }
+
+        }
+    }
+}
+
+impl FromStr for CommandLine {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match &s[2..4] {
+            "cd" => Ok(CommandLine::Cd(s[5..].to_string())),
+            "ls" => Ok(CommandLine::Ls),
+            _ => unreachable!()
+        }
+    }
+}
+
+impl FromStr for FileLine {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut split = s.split(" ");
+        let (dir_or_size, name) = (split.next().unwrap(), split.next().unwrap());
+
+        match dir_or_size {
+            "dir" => Ok(FileLine::Dir(name.to_string())),
+            _ => Ok(FileLine::File(name.to_string(), dir_or_size.parse::<u32>().unwrap()))
+
+        }
+    }
 }
